1bfd1bc63f35dc12878365971f74a2fff0957d94
walfud <walfud@aliyun.com>
2015-07-19 10:14:48 +0800

    .

---
+++ app/src/main/java/walfud/meetu/view/RadarView.java
@@ -0,0 +1,18 @@
+package walfud.meetu.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by song on 2015/7/18.
+ */
+public class RadarView extends View {
+    public RadarView(Context context) {
+        super(context);
+    }
+
+    public RadarView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
