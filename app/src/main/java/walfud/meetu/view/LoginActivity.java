3a0e48d135e57b1fe457ecf225a83cd54aa35ea2
walfud <walfud@aliyun.com>
2015-08-03 22:13:32 +0800

    LoginActivity ReFactor to MVP

--- app/src/main/java/walfud/meetu/view/LoginActivity.java
+++ app/src/main/java/walfud/meetu/view/LoginActivity.java
@@ -1,19 +1,16 @@
 package walfud.meetu.view;
 
 import android.app.Activity;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import org.meetu.client.handler.UserAccessHandler;
-import org.meetu.client.listener.UserAccessListener;
-import org.meetu.constant.Constant;
 import org.meetu.dto.UserAccessDto;
 import org.meetu.model.User;
 
 import walfud.meetu.R;
+import walfud.meetu.presenter.LoginPresenter;
 
 
 /**
@@ -26,10 +23,36 @@ public class LoginActivity extends Activity {
     private EditText mPhoneNum;
     private EditText mPassword;
 
+    private LoginPresenter mPresenter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mPresenter = new LoginPresenter(this);
+        mPresenter.setOnLoginListener(new LoginPresenter.OnLoginListener() {
+            @Override
+            public void onRegister(User user) {
+                Toast.makeText(LoginActivity.this,
+                        String.format("注册成功: id(%s)", user.getId()),
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onLogin(User user) {
+                Toast.makeText(LoginActivity.this,
+                        String.format("登陆成功: id(%s)", user.getId()),
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFail(UserAccessDto userAccessDto) {
+                Toast.makeText(LoginActivity.this,
+                        String.format("注册/登陆失败: %s(%s)", userAccessDto.getErrMsg(), userAccessDto.getErrCode()),
+                        Toast.LENGTH_LONG).show();
+            }
+        });
 
         mPhoneNum = (EditText) findViewById(R.id.phone_num);
         mPassword = (EditText) findViewById(R.id.password);
@@ -40,54 +63,6 @@ public class LoginActivity extends Activity {
         user.setMobile(mPhoneNum.getText().toString());
         user.setPwd(mPassword.getText().toString());
 
-        new AsyncTask<Void, Void, UserAccessDto>() {
-
-            private UserAccessDto mUserAccessDto;
-
-            @Override
-            protected UserAccessDto doInBackground(Void... params) {
-                try {
-                    new UserAccessHandler().onAccess(new UserAccessListener() {
-                        @Override
-                        public void access(UserAccessDto userAccessDto) {
-                            mUserAccessDto = userAccessDto;
-                        }
-                    }, user);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
-                return mUserAccessDto;
-            }
-
-            @Override
-            protected void onPostExecute(UserAccessDto userAccessDto) {
-                super.onPostExecute(userAccessDto);
-
-                if (false) {
-                } else if (Constant.ACCESS_STATUS_REG.equals(userAccessDto.getAccess_status())) {
-                    MainActivity.startActivity(LoginActivity.this);
-                    LoginActivity.this.finish();
-
-                    Toast.makeText(LoginActivity.this,
-                            String.format("注册成功: id(%s)", userAccessDto.getUser().getId()),
-                            Toast.LENGTH_SHORT).show();
-                } else if (Constant.ACCESS_STATUS_LOGIN.equals(userAccessDto.getAccess_status())) {
-                    MainActivity.startActivity(LoginActivity.this);
-                    LoginActivity.this.finish();
-
-                    Toast.makeText(LoginActivity.this,
-                            String.format("登陆成功: id(%s)", userAccessDto.getUser().getId()),
-                            Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(LoginActivity.this,
-                            String.format("注册/登陆失败: %s(%s)", userAccessDto.getErrMsg(), userAccessDto.getErrCode()),
-                            Toast.LENGTH_LONG).show();
-                }
-            }
-        }.execute();
-
-//        MainActivity.startActivity(LoginActivity.this);
-//        LoginActivity.this.finish();
+        mPresenter.onClickLogin(user);
     }
 }
