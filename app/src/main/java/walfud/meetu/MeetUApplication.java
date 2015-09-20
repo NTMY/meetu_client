f50e14b28d6993ad4282a732857a862098f38837
walfud <walfud@aliyun.com>
2015-09-20 13:23:18 +0800

    Add GreenDao

--- app/src/main/java/walfud/meetu/MeetUApplication.java
+++ app/src/main/java/walfud/meetu/MeetUApplication.java
@@ -5,6 +5,8 @@ import android.content.Context;
 
 import com.bugtags.library.Bugtags;
 
+import walfud.meetu.model.DaoMaster;
+
 /**
  * Created by song on 2015/6/22.
  */
@@ -13,10 +15,12 @@ public class MeetUApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        sApplicationContext = this;
 
         Bugtags.start("b27243a172339c9df358ab036868ec05", this, Bugtags.BTGInvocationEventBubble);
 
-        sApplicationContext = this;
+        // Create Db
+        DaoMaster.DevOpenHelper devOpenHelper = new DaoMaster.DevOpenHelper(this, "meetu", null);
     }
 
     private static Context sApplicationContext;
